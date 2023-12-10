6cc7a1d6bb24fd1b7ee5d5166e91a167729b23d0
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-12-10 12:18:43 -0300

    move constants to constants.py

--- flowmapper/unit.py
+++ flowmapper/unit.py
@@ -5,57 +5,12 @@ import flowmapper.jsonpath as jp
 from pint import UnitRegistry, errors
 import importlib.resources as resource
 from .utils import normalize_str
+from .constants import UNITS_NORMALIZATION
 
 ureg = UnitRegistry()
 
 with resource.as_file(resource.files('flowmapper').joinpath('data/units.txt')) as filepath:
     ureg.load_definitions(filepath)
-
-UNITS_NORMALIZATION = {
-    "a": "year",  # Common in LCA circles; could be confused with are
-    "bq": "becquerel",
-    "g": "gram",
-    "gj": "gigajoule",
-    "h": "hour",
-    "ha": "hectare",
-    "hr": "hour",
-    "kbq": "kilobecquerel",
-    "kg": "kilogram",
-    "kgkm": "kilogram_kilometer",
-    "km": "kilometer",
-    "kj": "kilojoule",
-    "kwh": "kilowatt hour",
-    "l": "litre",
-    "lu": "livestock unit",
-    "m": "meter",
-    "m*year": "meter_year",
-    "m2": "square_meter",
-    "m2*year": "square_meter_year",
-    "m2a": "square_meter_year",
-    "m2y": "square_meter_year",
-    "m3": "cubic_meter",
-    "m3*year": "cubic_meter_year",
-    "m3a": "cubic_meter_year",
-    "m3y": "cubic_meter_year",
-    "ma": "meter_year",
-    "metric ton*km": "ton_kilometer",
-    "mj": "megajoule",
-    "my": "meter_year",
-    "nm3": "cubic_meter",
-    "p": "unit",
-    "personkm": "person_kilometer",
-    "person*km": "person_kilometer",
-    "pkm": "person_kilometer",
-    "tkm": "ton_kilometer",
-    "vkm": "vehicle_kilometer",
-    'kg sw': "kilogram_separative_work_unit",
-    'km*year': "kilometer_year",
-    'metric ton*km': "ton_kilometer",
-    'person*km': "person_kilometer",
-    'wh': 'watt_hour',
-}
-
-normalize_unit = lambda x: UNITS_NORMALIZATION.get(x, x)
 
 @dataclass
 class Unit:
@@ -97,4 +52,5 @@ class Unit:
 
     @staticmethod
     def normalize(x):
-        return normalize_unit(normalize_str(x))
+        s = normalize_str(x)
+        return UNITS_NORMALIZATION.get(s, s)

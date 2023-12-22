aa02b277478c62cbdb8de02656155d11bbaebea8
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-12-21 21:36:03 -0300

    add snapshot tests with syrupy

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -69,8 +69,8 @@ def test_format_randonneur(tmp_path):
     assert expected_files == files
 
 
-def test_matched_flows(tmp_path):
-    result = runner.invoke(
+def test_matched_flows(tmp_path, snapshot):
+    runner.invoke(
         app,
         [
             "map",
@@ -84,101 +84,15 @@ def test_matched_flows(tmp_path):
             str(tmp_path),
         ],
     )
-    expected_files = sorted(
-        [
-            tmp_path / "sp-ei-3.7.json",
-            tmp_path / "sp-ei-3.7.xlsx",
-            tmp_path / "sp-ei-3.7-unmatched-source.json",
-            tmp_path / "sp-ei-3.7-matched-source.json",
-            tmp_path / "sp-ei-3.7-unmatched-target.json",
-            tmp_path / "sp-ei-3.7-matched-target.json",
-        ]
-    )
-
-    files = sorted(tmp_path.glob("**/*"))
-
-    with open(tmp_path / "sp-ei-3.7-unmatched-source.json") as fs:
-        unmatched_source = json.load(fs)
 
     with open(tmp_path / "sp-ei-3.7-matched-source.json") as fs:
-        matched_source = json.load(fs)
+        actual = json.load(fs)
 
-    with open(tmp_path / "sp-ei-3.7-unmatched-target.json") as fs:
-        unmatched_target = json.load(fs)
-
-    with open(tmp_path / "sp-ei-3.7-matched-target.json") as fs:
-        matched_target = json.load(fs)
-
-    assert unmatched_source == [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "high. pop."],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "Cesium-134",
-            "unit": "kBq",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-        {"name": "Cesium-134", "unit": "kBq", "categories": ["Emissions to soil", ""]},
-        {
-            "name": "Zinc, Zn 0.63%, Au 9.7E-4%, Ag 9.7E-4%, Cu 0.38%, Pb 0.014%, in ore",
-            "unit": "kg",
-            "categories": ["Resources", "in ground"],
-        },
-    ]
-    assert matched_source == [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "(unspecified)"],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "Ammonia, FR",
-            "unit": "kg",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-    ]
-    assert unmatched_target == []
-    assert matched_target == [
-        {
-            "@id": "09db39be-d9a6-4fc3-8d25-1f80b23e9131",
-            "@unitId": "487df68b-4994-4027-8fdc-a4dc298257b7",
-            "@casNumber": "000110-63-4",
-            "name": {"@xml:lang": "en", "#text": "1,4-Butanediol"},
-            "unitName": {"@xml:lang": "en", "#text": "kg"},
-            "compartment": {
-                "@subcompartmentId": "7011f0aa-f5f9-4901-8c10-884ad8296812",
-                "compartment": {"@xml:lang": "en", "#text": "air"},
-                "subcompartment": {"@xml:lang": "en", "#text": "unspecified"},
-            },
-            "synonym": {"@xml:lang": "en", "#text": "Butylene glycol"},
-        },
-        {
-            "@id": "0f440cc0-0f74-446d-99d6-8ff0e97a2444",
-            "@unitId": "487df68b-4994-4027-8fdc-a4dc298257b7",
-            "@casNumber": "007664-41-7",
-            "name": {"@xml:lang": "en", "#text": "Ammonia"},
-            "unitName": {"@xml:lang": "en", "#text": "kg"},
-            "compartment": {
-                "@subcompartmentId": "be7e06e9-0bf5-462e-99dc-fe4aee383c48",
-                "compartment": {"@xml:lang": "en", "#text": "air"},
-                "subcompartment": {
-                    "@xml:lang": "en",
-                    "#text": "non-urban air or from high stacks",
-                },
-            },
-        },
-    ]
-
-    assert result.exit_code == 0
-    assert expected_files == files
+    assert actual == snapshot
 
 
-def test_matched_flows_with_randonneur_transformations(tmp_path):
-    result = runner.invoke(
+def test_matched_flows_with_randonneur_transformations(tmp_path, snapshot):
+    runner.invoke(
         app,
         [
             "map",
@@ -194,101 +108,15 @@ def test_matched_flows_with_randonneur_transformations(tmp_path):
             str(tmp_path),
         ],
     )
-    expected_files = sorted(
-        [
-            tmp_path / "sp-ei-3.7.json",
-            tmp_path / "sp-ei-3.7.xlsx",
-            tmp_path / "sp-ei-3.7-unmatched-source.json",
-            tmp_path / "sp-ei-3.7-matched-source.json",
-            tmp_path / "sp-ei-3.7-unmatched-target.json",
-            tmp_path / "sp-ei-3.7-matched-target.json",
-        ]
-    )
-
-    files = sorted(tmp_path.glob("**/*"))
-
-    with open(tmp_path / "sp-ei-3.7-unmatched-source.json") as fs:
-        unmatched_source = json.load(fs)
 
     with open(tmp_path / "sp-ei-3.7-matched-source.json") as fs:
-        matched_source = json.load(fs)
+        actual = json.load(fs)
 
-    with open(tmp_path / "sp-ei-3.7-unmatched-target.json") as fs:
-        unmatched_target = json.load(fs)
-
-    with open(tmp_path / "sp-ei-3.7-matched-target.json") as fs:
-        matched_target = json.load(fs)
-
-    assert unmatched_source == [
-        {
-            "name": "Cesium-134",
-            "unit": "kBq",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-        {"name": "Cesium-134", "unit": "kBq", "categories": ["Emissions to soil", ""]},
-        {
-            "name": "Zinc, Zn 0.63%, Au 9.7E-4%, Ag 9.7E-4%, Cu 0.38%, Pb 0.014%, in ore",
-            "unit": "kg",
-            "categories": ["Resources", "in ground"],
-        },
-    ]
-    assert matched_source == [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "(unspecified)"],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "high. pop."],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "Ammonia, FR",
-            "unit": "kg",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-    ]
-    assert unmatched_target == []
-    assert matched_target == [
-        {
-            "@id": "09db39be-d9a6-4fc3-8d25-1f80b23e9131",
-            "@unitId": "487df68b-4994-4027-8fdc-a4dc298257b7",
-            "@casNumber": "000110-63-4",
-            "name": {"@xml:lang": "en", "#text": "1,4-Butanediol"},
-            "unitName": {"@xml:lang": "en", "#text": "kg"},
-            "compartment": {
-                "@subcompartmentId": "7011f0aa-f5f9-4901-8c10-884ad8296812",
-                "compartment": {"@xml:lang": "en", "#text": "air"},
-                "subcompartment": {"@xml:lang": "en", "#text": "unspecified"},
-            },
-            "synonym": {"@xml:lang": "en", "#text": "Butylene glycol"},
-        },
-        {
-            "@id": "0f440cc0-0f74-446d-99d6-8ff0e97a2444",
-            "@unitId": "487df68b-4994-4027-8fdc-a4dc298257b7",
-            "@casNumber": "007664-41-7",
-            "name": {"@xml:lang": "en", "#text": "Ammonia"},
-            "unitName": {"@xml:lang": "en", "#text": "kg"},
-            "compartment": {
-                "@subcompartmentId": "be7e06e9-0bf5-462e-99dc-fe4aee383c48",
-                "compartment": {"@xml:lang": "en", "#text": "air"},
-                "subcompartment": {
-                    "@xml:lang": "en",
-                    "#text": "non-urban air or from high stacks",
-                },
-            },
-        },
-    ]
-
-    assert result.exit_code == 0
-    assert expected_files == files
+    assert actual == snapshot
 
 
-def test_matched_flows_with_multiple_randonneur_transformations(tmp_path):
-    result = runner.invoke(
+def test_matched_flows_with_multiple_randonneur_transformations(tmp_path, snapshot):
+    runner.invoke(
         app,
         [
             "map",
@@ -306,94 +134,8 @@ def test_matched_flows_with_multiple_randonneur_transformations(tmp_path):
             str(tmp_path),
         ],
     )
-    expected_files = sorted(
-        [
-            tmp_path / "sp-ei-3.7.json",
-            tmp_path / "sp-ei-3.7.xlsx",
-            tmp_path / "sp-ei-3.7-unmatched-source.json",
-            tmp_path / "sp-ei-3.7-matched-source.json",
-            tmp_path / "sp-ei-3.7-unmatched-target.json",
-            tmp_path / "sp-ei-3.7-matched-target.json",
-        ]
-    )
-
-    files = sorted(tmp_path.glob("**/*"))
-
-    with open(tmp_path / "sp-ei-3.7-unmatched-source.json") as fs:
-        unmatched_source = json.load(fs)
 
     with open(tmp_path / "sp-ei-3.7-matched-source.json") as fs:
-        matched_source = json.load(fs)
+        actual = json.load(fs)
 
-    with open(tmp_path / "sp-ei-3.7-unmatched-target.json") as fs:
-        unmatched_target = json.load(fs)
-
-    with open(tmp_path / "sp-ei-3.7-matched-target.json") as fs:
-        matched_target = json.load(fs)
-
-    assert unmatched_source == [
-        {
-            "name": "Cesium-134",
-            "unit": "kBq",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-        {"name": "Cesium-134", "unit": "kBq", "categories": ["Emissions to soil", ""]},
-        {
-            "name": "Zinc, Zn 0.63%, Au 9.7E-4%, Ag 9.7E-4%, Cu 0.38%, Pb 0.014%, in ore",
-            "unit": "kg",
-            "categories": ["Resources", "in ground"],
-        },
-    ]
-    assert matched_source == [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "(unspecified)"],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "high. pop."],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "Ammonia, FR",
-            "unit": "kg",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-    ]
-    assert unmatched_target == []
-    assert matched_target == [
-        {
-            "@id": "09db39be-d9a6-4fc3-8d25-1f80b23e9131",
-            "@unitId": "487df68b-4994-4027-8fdc-a4dc298257b7",
-            "@casNumber": "000110-63-4",
-            "name": {"@xml:lang": "en", "#text": "1,4-Butanediol"},
-            "unitName": {"@xml:lang": "en", "#text": "kg"},
-            "compartment": {
-                "@subcompartmentId": "7011f0aa-f5f9-4901-8c10-884ad8296812",
-                "compartment": {"@xml:lang": "en", "#text": "air"},
-                "subcompartment": {"@xml:lang": "en", "#text": "unspecified"},
-            },
-            "synonym": {"@xml:lang": "en", "#text": "Butylene glycol"},
-        },
-        {
-            "@id": "0f440cc0-0f74-446d-99d6-8ff0e97a2444",
-            "@unitId": "487df68b-4994-4027-8fdc-a4dc298257b7",
-            "@casNumber": "007664-41-7",
-            "name": {"@xml:lang": "en", "#text": "Ammonia"},
-            "unitName": {"@xml:lang": "en", "#text": "kg"},
-            "compartment": {
-                "@subcompartmentId": "be7e06e9-0bf5-462e-99dc-fe4aee383c48",
-                "compartment": {"@xml:lang": "en", "#text": "air"},
-                "subcompartment": {
-                    "@xml:lang": "en",
-                    "#text": "non-urban air or from high stacks",
-                },
-            },
-        },
-    ]
-
-    assert result.exit_code == 0
-    assert expected_files == files
+    assert actual == snapshot
--- tests/test_flowmap.py
+++ tests/test_flowmap.py
@@ -5,7 +5,6 @@ from flowmapper.match import (
     match_identical_names,
 )
 
-
 def test_flowmap_mappings(source_flows, target_flows):
     flowmap = Flowmap(source_flows, target_flows)
     actual = flowmap.mappings[0]
@@ -20,77 +19,26 @@ def test_flowmap_mappings(source_flows, target_flows):
     assert actual["match_rule"] == "match_identical_names"
 
 
-def test_flowmap_to_randonneur(source_flows, target_flows):
+def test_flowmap_to_randonneur(source_flows, target_flows, snapshot):
     flowmap = Flowmap(source_flows, target_flows)
     actual = flowmap.to_randonneur()
-    expected = [
-        {
-            "source": {
-                "name": "1,4-Butanediol",
-                "categories": ["Air", "(unspecified)"],
-                "unit": "kg",
-            },
-            "target": {
-                "uuid": "09db39be-d9a6-4fc3-8d25-1f80b23e9131",
-                "name": "1,4-Butanediol",
-                "context": "air/unspecified",
-                "unit": "kg",
-            },
-            "conversion_factor": 1.0,
-            "comment": "Identical names",
-        },
-        {
-            "source": {
-                "name": "Ammonia, FR",
-                "categories": ["Emissions to air", "low. pop."],
-                "unit": "kg",
-            },
-            "target": {
-                "uuid": "0f440cc0-0f74-446d-99d6-8ff0e97a2444",
-                "name": "Ammonia",
-                "context": "air/non-urban air or from high stacks",
-                "unit": "kg",
-                "location": "FR",
-            },
-            "conversion_factor": 1.0,
-            "comment": "Names with country code",
-        },
-    ]
-    assert actual == expected
+    assert actual == snapshot
 
 
-def test_flowmap_with_custom_rules_no_match(source_flows, target_flows):
+def test_flowmap_with_custom_rules_no_match(source_flows, target_flows, snapshot):
     flowmap = Flowmap(
         source_flows,
         target_flows,
         rules=[match_emissions_with_suffix_ion],
     )
     actual = flowmap.mappings
-    expected = []
-    assert actual == expected
+    assert actual == snapshot
 
 
-def test_flowmap_with_custom_rules_match(source_flows, target_flows):
+def test_flowmap_with_custom_rules_match(source_flows, target_flows, snapshot):
     flowmap = Flowmap(source_flows, target_flows, rules=[match_identical_names])
     actual = flowmap.to_randonneur()
-    expected = [
-        {
-            "source": {
-                "name": "1,4-Butanediol",
-                "categories": ["Air", "(unspecified)"],
-                "unit": "kg",
-            },
-            "target": {
-                "uuid": "09db39be-d9a6-4fc3-8d25-1f80b23e9131",
-                "name": "1,4-Butanediol",
-                "context": "air/unspecified",
-                "unit": "kg",
-            },
-            "conversion_factor": 1.0,
-            "comment": "Identical names",
-        }
-    ]
-    assert actual == expected
+    assert actual == snapshot
 
 
 def test_flowmap_to_glad(source_flows, target_flows):
@@ -118,256 +66,44 @@ def test_flowmap_to_glad(source_flows, target_flows):
     assert actual.equals(pd.DataFrame(expected))
 
 
-def test_flowmap_export_matched(source_flows, target_flows):
+def test_flowmap_export_matched(source_flows, target_flows, snapshot):
     flowmap = Flowmap(source_flows, target_flows)
     actual = [flow.raw for flow in flowmap.matched_source]
-    expected = [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "(unspecified)"],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "Ammonia, FR",
-            "unit": "kg",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-    ]
-    assert actual == expected
+    assert actual == snapshot
 
-
-def test_flowmap_export_unmatched(source_flows, target_flows):
+def test_flowmap_export_unmatched(source_flows, target_flows, snapshot):
     flowmap = Flowmap(source_flows, target_flows)
     actual = [flow.raw for flow in flowmap.unmatched_source]
-    expected = [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "high. pop."],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "Cesium-134",
-            "unit": "kBq",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-        {"name": "Cesium-134", "unit": "kBq", "categories": ["Emissions to soil", ""]},
-        {
-            "name": "Zinc, Zn 0.63%, Au 9.7E-4%, Ag 9.7E-4%, Cu 0.38%, Pb 0.014%, in ore",
-            "unit": "kg",
-            "categories": ["Resources", "in ground"],
-        },
-    ]
-    assert actual == expected
+    assert actual == snapshot
 
-
-def test_flowmap_nomatch_rule(source_flows, target_flows):
+def test_flowmap_nomatch_rule(source_flows, target_flows, snapshot):
     nomatch = lambda flow: flow.context.value == "air/urban air close to ground"
     flowmap = Flowmap(source_flows, target_flows, nomatch_rules=[nomatch])
 
-    actual_source_flows_nomatch = [flow.raw for flow in flowmap.source_flows_nomatch]
-    expected_source_flows_nomatch = [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "high. pop."],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        }
-    ]
-    assert actual_source_flows_nomatch == expected_source_flows_nomatch
+    actual = [flow.raw for flow in flowmap.source_flows_nomatch]
+    assert actual == snapshot
 
-    actual_source_flows = [flow.raw for flow in flowmap.source_flows]
-    expected_source_flows = [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "(unspecified)"],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "Cesium-134",
-            "unit": "kBq",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-        {"name": "Cesium-134", "unit": "kBq", "categories": ["Emissions to soil", ""]},
-        {
-            "name": "Zinc, Zn 0.63%, Au 9.7E-4%, Ag 9.7E-4%, Cu 0.38%, Pb 0.014%, in ore",
-            "unit": "kg",
-            "categories": ["Resources", "in ground"],
-        },
-        {
-            "name": "Ammonia, FR",
-            "unit": "kg",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-    ]
-    assert actual_source_flows == expected_source_flows
-
-
-def test_flowmap_nomatch_rule_false(source_flows, target_flows):
+def test_flowmap_nomatch_rule_false(source_flows, target_flows, snapshot):
     nomatch = lambda flow: flow.context.value == "water"
     flowmap = Flowmap(source_flows, target_flows, nomatch_rules=[nomatch])
 
-    actual_source_flows_nomatch = [flow.raw for flow in flowmap.source_flows_nomatch]
-    expected_source_flows_nomatch = []
-    assert actual_source_flows_nomatch == expected_source_flows_nomatch
+    actual = [flow.raw for flow in flowmap.source_flows]
+    assert actual == snapshot
 
-    actual_source_flows = [flow.raw for flow in flowmap.source_flows]
-    expected_source_flows = [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "(unspecified)"],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "high. pop."],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "Cesium-134",
-            "unit": "kBq",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-        {"name": "Cesium-134", "unit": "kBq", "categories": ["Emissions to soil", ""]},
-        {
-            "name": "Zinc, Zn 0.63%, Au 9.7E-4%, Ag 9.7E-4%, Cu 0.38%, Pb 0.014%, in ore",
-            "unit": "kg",
-            "categories": ["Resources", "in ground"],
-        },
-        {
-            "name": "Ammonia, FR",
-            "unit": "kg",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-    ]
-    assert actual_source_flows == expected_source_flows
-
-
-def test_flowmap_nomatch_multiple_rules(source_flows, target_flows):
+def test_flowmap_nomatch_multiple_rules(source_flows, target_flows, snapshot):
     nomatch1 = lambda flow: flow.context.value == "air/urban air close to ground"
     nomatch2 = lambda flow: flow.context.value == "air"
     flowmap = Flowmap(source_flows, target_flows, nomatch_rules=[nomatch1, nomatch2])
 
-    actual_source_flows_nomatch = [flow.raw for flow in flowmap.source_flows_nomatch]
-    expected_source_flows_nomatch = [
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "(unspecified)"],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-        {
-            "name": "1,4-Butanediol",
-            "categories": ["Air", "high. pop."],
-            "unit": "kg",
-            "CAS": "000110-63-4",
-        },
-    ]
-    assert actual_source_flows_nomatch == expected_source_flows_nomatch
+    actual = [flow.raw for flow in flowmap.source_flows_nomatch]
+    assert actual == snapshot
 
-    actual_source_flows = [flow.raw for flow in flowmap.source_flows]
-    expected_source_flows = [
-        {
-            "name": "Cesium-134",
-            "unit": "kBq",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-        {"name": "Cesium-134", "unit": "kBq", "categories": ["Emissions to soil", ""]},
-        {
-            "name": "Zinc, Zn 0.63%, Au 9.7E-4%, Ag 9.7E-4%, Cu 0.38%, Pb 0.014%, in ore",
-            "unit": "kg",
-            "categories": ["Resources", "in ground"],
-        },
-        {
-            "name": "Ammonia, FR",
-            "unit": "kg",
-            "categories": ["Emissions to air", "low. pop."],
-        },
-    ]
-    assert actual_source_flows == expected_source_flows
-
-
-def test_flowmap_mappings_ei_ei(target_flows):
+def test_flowmap_mappings_ei_ei(target_flows, snapshot):
     flowmap = Flowmap(target_flows, target_flows)
     actual = flowmap.to_randonneur()
-    expected = [
-        {
-            "source": {
-                "name": {"@xml:lang": "en", "#text": "1,4-Butanediol"},
-                "compartment": {
-                    "@subcompartmentId": "7011f0aa-f5f9-4901-8c10-884ad8296812",
-                    "compartment": {"@xml:lang": "en", "#text": "air"},
-                    "subcompartment": {"@xml:lang": "en", "#text": "unspecified"},
-                },
-                "unitName": {"@xml:lang": "en", "#text": "kg"},
-                "@id": "09db39be-d9a6-4fc3-8d25-1f80b23e9131",
-            },
-            "target": {
-                "uuid": "09db39be-d9a6-4fc3-8d25-1f80b23e9131",
-                "name": "1,4-Butanediol",
-                "context": "air/unspecified",
-                "unit": "kg",
-            },
-            "conversion_factor": 1.0,
-            "comment": "Identical uuid",
-        },
-        {
-            "source": {
-                "name": {"@xml:lang": "en", "#text": "Ammonia"},
-                "compartment": {
-                    "@subcompartmentId": "be7e06e9-0bf5-462e-99dc-fe4aee383c48",
-                    "compartment": {"@xml:lang": "en", "#text": "air"},
-                    "subcompartment": {
-                        "@xml:lang": "en",
-                        "#text": "non-urban air or from high stacks",
-                    },
-                },
-                "unitName": {"@xml:lang": "en", "#text": "kg"},
-                "@id": "0f440cc0-0f74-446d-99d6-8ff0e97a2444",
-            },
-            "target": {
-                "uuid": "0f440cc0-0f74-446d-99d6-8ff0e97a2444",
-                "name": "Ammonia",
-                "context": "air/non-urban air or from high stacks",
-                "unit": "kg",
-            },
-            "conversion_factor": 1.0,
-            "comment": "Identical uuid",
-        },
-    ]
-    assert actual == expected
+    assert actual == snapshot
 
-
-def test_flowmap_mappings_ei39_ei310(flows_ei39, flows_ei310):
+def test_flowmap_mappings_ei39_ei310(flows_ei39, flows_ei310, snapshot):
     flowmap = Flowmap(flows_ei39, flows_ei310)
     actual = flowmap.to_randonneur()
-    expected = [
-        {
-            "source": {
-                "name": {"@xml:lang": "en", "#text": "2,4-D amines"},
-                "compartment": {
-                    "@subcompartmentId": "be7e06e9-0bf5-462e-99dc-fe4aee383c48",
-                    "compartment": {"@xml:lang": "en", "#text": "air"},
-                    "subcompartment": {
-                        "@xml:lang": "en",
-                        "#text": "non-urban air or from high stacks",
-                    },
-                },
-                "unitName": {"@xml:lang": "en", "#text": "kg"},
-                "@id": "4f777e05-70f9-4a18-a406-d8232325073f",
-            },
-            "target": {
-                "uuid": "b6b4201e-0561-5992-912f-e729fbf04e41",
-                "name": "2,4-D dimethylamine salt",
-                "context": "air/non-urban air or from high stacks",
-                "unit": "kg",
-            },
-            "conversion_factor": 1.0,
-            "comment": "Identical uuid",
-        }
-    ]
-    assert actual == expected
+    assert actual == snapshot
